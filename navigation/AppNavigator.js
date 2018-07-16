dce817da2cc63d4921d8eeee735eef1991cffc29
Leandro Lopez <lopezlean@gmail.com>
2018-07-16 10:21:06 -0300

    update

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
+
 
 import MainTabNavigator from './MainTabNavigator';
+
+import LoginScreen from '../screens/LoginScreen';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+
+
+const AuthStack = createStackNavigator({ Login: LoginScreen });
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  AuthLoading: AuthLoadingScreen,
   Main: MainTabNavigator,
+  Auth: AuthStack
+
+},
+{
+  initialRouteName: 'AuthLoading',
 });

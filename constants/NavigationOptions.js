4d06b25c6f2a8e0e43bff56c248c7c41b8fb9f9b
Leandro Lopez <lopezlean@Leandros-iMac.local>
2018-07-21 14:37:20 -0300

    update

--- constants/NavigationOptions.js
+++ constants/NavigationOptions.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import {TouchableOpacity} from 'react-native';
-import {Icon} from 'native-base';
+import React, {Component} from 'react';
+
+import {TouchableOpacity,StyleSheet, View, Text, Image} from 'react-native';
+import {Icon, Header} from 'native-base';
+
+
+
+import superligaAppImg from '../assets/images/app_logo.png';
+import superligaImg from '../assets/images/logo.png';
+
+
+class HeaderLeft extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={superligaImg} style={styles.image} />
+      </View>
+    );
+  }
+}
+
+
 
 export default {
     navigationOptions: ({ navigation }) => ({
+      headerLeft: <Image source={superligaImg} style={styles.superligaImg} />,
+      headerTitle: <Image source={superligaAppImg} style={styles.superligaAppImg} />,
       headerRight: <TouchableOpacity  onPress={() => {navigation.openDrawer();}}><Icon name="ios-menu" style={{ paddingRight: 10 }} /></TouchableOpacity>,
       headerStyle: {
-        backgroundColor: 'gray',
+        backgroundColor: 'transparent',
+        height: 133,
+        borderBottomWidth: 0 // removes the border on the bottom
+
       },
       headerTintColor: '#fff',
       headerTitleStyle: {
@@ -14,3 +38,26 @@ export default {
       },
     })
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    superligaAppImg: {
+      width: 181,
+      height: 133,
+    },
+    superligaImg: {
+      width: 68,
+      height: 69,
+    },
+    text: {
+      color: 'white',
+      fontWeight: 'bold',
+      backgroundColor: 'transparent',
+      marginTop: 20,
+    },
+  });
+  

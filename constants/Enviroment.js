7c0d7c487848cb54f4572c6e7113c3817e485b64
Leandro Lopez <lopezlean@Leandros-iMac.local>
2018-08-03 17:04:08 -0300

    update

--- constants/Enviroment.js
+++ constants/Enviroment.js
@@ -23,7 +23,7 @@ const ENV = {
 }
 
 function getEnvVars(env = '') {
-  if (env === null || env === undefined || env === '') return {...ENV.dev,channel:'local-development'}
+  if (env === null || env === undefined || env === '') return {...ENV.dev,channel:'local-development 0.1'}
   if (env.indexOf('dev') !== -1) return {...ENV.dev,channel:env}
   if (env.indexOf('staging') !== -1) return {...ENV.staging,channel:env}
   if (env.indexOf('prod') !== -1) return {...ENV.prod,channel:env}
